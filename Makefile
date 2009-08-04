5bd0a55b6b7dca0b980f539ce80938df563d53d7
visionmedia <tj@vision-media.ca>
2009-08-04 11:37:51 -0700

    Fixing build

--- Makefile
+++ Makefile
@@ -6,15 +6,17 @@ all: clean parser build
   
 clean:
 	rm -fr build
+	rm -fr bin
   
 build:
-	mkdir build
-	$(CC) src/*.c -std=c99 -o ./build/cspec
+	mkdir build bin
+	$(CC) -c src/cspec.c -std=c99 -o ./build/cspec.o
+	$(CC) src/*.c -std=c99 -o ./bin/cspec
 
 parser:
 	$(LEG) src/parser.leg > src/parser.c
 	
 test: all
-	./build/cspec < spec/hook_spec.c  > spec/hook.c
-	$(CC) spec/hook.c -I src -o ./build/test
-	./build/test
+	./bin/cspec < spec/hook_spec.c  > spec/hook.c
+	$(CC) ./build/cspec.o spec/hook.c -std=c99 -I src -o ./bin/test
+	./bin/test
---
+++ spec/hook.c
@@ -0,0 +1,80 @@
+
+#include "cspec.h"
+
+static int nbefore = 0;
+static int nbefore_each = 0;
+static int nafter = 0;
+static int nafter_each = 0;
+
+
+ void block_0_callback() {
+    ++nbefore;
+ 
+}
+  
+ void block_1_callback() {
+    ++nafter;
+ 
+}
+  
+  
+    void block_2_callback() {
+
+      expect(nbefore == 1);
+   
+}
+    
+    void block_3_callback() {
+
+      expect(nbefore == 1);
+   
+}
+   
+  
+    void block_4_callback() {
+
+      expect(nafter == 1);
+   
+}
+
+    
+    void block_5_callback() {
+
+      expect(nafter == 1);
+   
+}
+ int main() {
+Suite *suite_0 = Suite_new("CSpec");
+
+Block *block_0 = Block_new(blockTypeBefore, NULL, &block_0_callback);
+
+Suite_push_block(suite_0, block_0);
+
+Block *block_1 = Block_new(blockTypeAfter, NULL, &block_1_callback);
+
+Suite_push_block(suite_0, block_1);
+
+Suite *suite_1 = Suite_new("before");
+
+Block *block_2 = Block_new(blockTypeSpec, "should be called before each suite", &block_2_callback);
+
+Suite_push_block(suite_1, block_2);
+
+Block *block_3 = Block_new(blockTypeSpec, "should not be called before each spec", &block_3_callback);
+
+Suite_push_block(suite_1, block_3);
+
+Suite *suite_2 = Suite_new("after");
+
+Block *block_4 = Block_new(blockTypeSpec, "be called after each suite", &block_4_callback);
+
+Suite_push_block(suite_2, block_4);
+
+Block *block_5 = Block_new(blockTypeSpec, "should not be called after each spec", &block_5_callback);
+
+Suite_push_block(suite_2, block_5);
+
+
+Suite_run(suite_0);
+return 0;
+}
